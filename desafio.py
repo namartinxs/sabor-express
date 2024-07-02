8491e5daa0062be4cd95a69e780c4b2774163396
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-07-02 19:56:53 -0300

    desafio1

--- desafio.py
+++ desafio.py
@@ -1,12 +1,6 @@
-print('Python na escola de Programação Alura\n')
-nome = input('Insira seu nome')
-idade = input('Insira sua idade')
-print(f'Meu nome é {nome} e tenho {idade} anos \n')
-print('A','L','U','R','A', sep='\n')
+numero = int(input('Insira um número'));
 
-
-pi = 3.14159
-#f strings
-print(f'O valor de pi arredondado é: {pi:.2f}')
-#funcao round
-print ('O valor de pi arredondado é: ', round(pi,2))
+if numero % 2 == 0:
+    print(f'O número {numero} é par.')
+else:
+     print(f'O número {numero} é impar.')
