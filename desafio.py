3f3e81d90c0d73645f730beffa26f46ee8c94c02
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-07-02 17:04:04 -0300

    Desafio1

---
+++ desafio.py
@@ -0,0 +1,12 @@
+print('Python na escola de Programação Alura\n')
+nome = input('Insira seu nome')
+idade = input('Insira sua idade')
+print(f'Meu nome é {nome} e tenho {idade} anos \n')
+print('A','L','U','R','A', sep='\n')
+
+
+pi = 3.14159
+#f strings
+print(f'O valor de pi arredondado é: {pi:.2f}')
+#funcao round
+print ('O valor de pi arredondado é: ', round(pi,2))
