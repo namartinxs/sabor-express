9dd3d41464637b6b910b37bf21caf2379c023f6e
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-07-02 15:45:24 -0300

    Aula 1

---
+++ app.py
@@ -0,0 +1,12 @@
+print("""𝕊𝕒𝕓𝕠𝕣 𝕖𝕩𝕡𝕣𝕖𝕤𝕤
+
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')#cadastrado na base mas fora de visualização
+print('4. Sair\n')
+
+opcao_escolhida = input('Esolha uma opção:')
+#print('Você escolheu a opçao', opcao_escolhida)
+print(f'Você escolheu a opção: {opcao_escolhida}')
