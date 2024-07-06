2c26d21dc6771ad428b0767a1deea4e65922d21d
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-07-06 01:32:33 -0300

    desafio 1 e 2

---
+++ atv.py
@@ -0,0 +1,9 @@
+cidadao= {'nome':'Ju', 'idade': 18, 'cidade':'Cachoeirinha'}
+#atualizar idade 
+cidadao['idade']=19
+
+#adicionando 
+cidadao['profissão'] = 'programadora'
+
+#removendo
+del cidadao['cidade']
